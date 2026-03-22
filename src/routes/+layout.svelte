02a114df2f02f9c03d87e65160801212e45e772d
alchemistlowkey <alchemistlowkey@gmail.com>
2026-03-22 03:12:10 +0100

    Added meta tags and social media metadata to +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -46,7 +46,34 @@
   });
 
   let { children } = $props();
+
+  const title = "Lucky Samuel | Backend Engineer";
+  const description = "Backend Engineer | .NET / C#";
 </script>
+
+<svelte:head>
+  <meta name="description" content={description} />
+  <link rel="canonical" href="https://lucky-samuel.vercel.app" />
+  <meta name="image" content="https://lucky-samuel.vercel.app/logo.png" />
+  <meta property="og:title" content={title} />
+  <meta property="og:description" content={description} />
+  <meta property="og:url" content="https://lucky-samuel.vercel.app" />
+  <meta property="og:type" content="website" />
+  <meta
+    property="og:image"
+    content="https://lucky-samuel.vercel.app/logo.png"
+  />
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta property="twitter:url" content="https://x.com/alchemistlowkey" />
+  <meta property="twitter:title" content="Square Metres" />
+  <meta property="twitter:description" content={description} />
+  <meta
+    property="twitter:image"
+    content="https://lucky-samuel.vercel.app/logo.png"
+  />
+  <meta name="twitter:site" content="@alchemistlowkey" />
+  <meta name="twitter:creator" content="@alchemistlowkey" />
+</svelte:head>
 
 <div id="cursor-dot" aria-hidden="true"></div>
 <div id="cursor-ring" aria-hidden="true"></div>
